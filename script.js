499aa25ac3a1761e799be9cee3e0b99a35d0380a
MaxPopovschii <maxpopovschii@gmail.com>
2023-03-11 12:32:23 +0100

    changed email post method

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function sendEmail() {
+    var recipient = "maxpopovschii@gmail.com"
+    var subject = document.getElementById("subject").value;
+    var body = document.getElementById("body").value;
+    var mailtoLink = "mailto:" + recipient + "?subject=" + subject + "&body=" + body;
+    window.location.href = mailtoLink;
+  }
+  
